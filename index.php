4b2f3edc0c19455ad6461ed73828aa67cb4da6a9
svensgwen <svensgwen@gmail.com>
2026-03-13 01:46:01 +0530

    Added Favicon

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ foreach (scandir($baseDir) as $item) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="./assets/plugins/bootstrap-5.3.8/css/bootstrap.min.css">
-    <link rel="shortcut icon" href="" type="image/x-icon">
+    <link rel="shortcut icon" href="./assets/img/svg/robot.svg" type="image/x-icon">
 
     <link rel="stylesheet" href="./assets/css/style.css">
     <style>
